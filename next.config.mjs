5aa8bf9015bc860895e10428cc5e7a8e3c8ea721
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2024-04-09 13:56:18 +0530

    Header Created

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [{ protocol: "https", hostname: "tailwindui.com" }],
+  },
+};
 
 export default nextConfig;
